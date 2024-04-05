df4901d7601f6084353ad3480998d2475208608a
santatramcd <andrianjakasantatra22@gmail.com>
2024-04-05 11:53:19 +0300

    first modif home

---
+++ src/views/Home/HomeHead.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="container mt-5">
+      <h1>
+        AN{{ titleStyled }}
+      </h1>
+      <span class="span">Développeur Web#</span>
+      <span class="span-hr"></span>
+    </div>
+  </div>
+</template>
+<script setup>
+AOS.init({
+  offset: 400,
+  duration: 1000,
+});
+
+import { onMounted, ref } from "vue";
+// import Title from "@/components/Common/Title.vue";
+
+const titleStyled = ref("DRIANJAKA SANTATRA");
+
+onMounted(() => {
+  clearText();
+});
+
+function clearText() {
+  const contenu = titleStyled.value;
+  titleStyled.value = "";
+
+  let index = contenu.length;
+  const effacement = setInterval(() => {
+    index--;
+    titleStyled.value = contenu.slice(0, index);
+    if (index === 0) {
+      clearInterval(effacement);
+      setTimeout(writeText, 1000);
+    }
+  }, 150);
+}
+
+function writeText() {
+  const contenu = "DRIANJAKA SANTATRA";
+  let index = 0;
+  const ecriture = setInterval(() => {
+    index++;
+    titleStyled.value = contenu.slice(0, index);
+    if (index === contenu.length) {
+      clearInterval(ecriture);
+      setTimeout(clearText, 1000);
+    }
+  }, 150);
+}
+</script>
+<style scoped lang="scss">
+$colorblack: var(--token-d06c6878-f8de-4d7e-a60a-d0bb19103013, #141414);
+$colorwhite: var(--token-585607e0-d916-446c-9a5c-fad6aacfe8e9, #ffffff);
+$param: 20px;
+h1 {
+  font-size: 10vw;
+  font-family: "PP Right Grotesk Tight Bold", sans-serif;
+  font-weight: bolder;
+  color: $colorwhite;
+  text-align: center;
+  height: 25vw;
+}
+
+.span {
+  color: $colorwhite;
+  font-size: 20px;
+  text-align: center;
+  display: block;
+  // background-color: red;
+  font-weight: 600;
+}
+.span-hr {
+  width: 173px;
+  height: 2px;
+  background-color: $colorwhite;
+  margin: auto;
+  display: flex;
+}
+</style>
---
+++ src/views/Home/HomeTechnology.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 mt-3"  v-for="(item, index) in data" :key="index">
+          <!-- Contenu de la première colonne -->
+          <div class="divcard">
+            <div class="image">
+              <img
+                :src="item.img"
+                alt="icon"
+              />
+            </div>
+            <div class="colorname">
+              <h3 class="title-card">{{ item.title }}</h3>
+              <p>{{ item.description }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+const data =[
+    {
+        img : "https://rockresearch.com/wp-content/uploads/2017/11/event_technology.jpg",
+        title : 'Technology',
+        description : 'Lorem ipsum dolor sit amet.',
+    },
+    {
+        img : "https://www.skillstork.org/blog/wp-content/uploads/2022/11/modern-education-Skillstork-1568x882.jpg",
+        title : 'Virtualization',
+        description : 'Lorem ipsum dolor sit amet.',
+    },
+    {
+        img : "https://www.globalfocusmagazine.com/wp-content/uploads/2020/02/Engaging_with_technology-scaled.jpg",
+        title : 'Artificial Intelligence',
+        description : 'Lorem ipsum dolor sit amet.',
+    }
+]
+</script>
+<style scoped lang="scss">
+$colorblack: var(--token-d06c6878-f8de-4d7e-a60a-d0bb19103013, #141414);
+$colorwhite: var(--token-585607e0-d916-446c-9a5c-fad6aacfe8e9, #ffffff);
+$param: 20px;
+.container{
+    margin-top: 150px;
+    // background: red;
+}
+img {
+  width: 100%;
+  height: 100%;
+  // background-color: aqua;
+}
+.row {
+  align-items: stretch;
+  position: relative;
+
+}
+.divcard {
+  background-color: $colorblack;
+  border: 1px solid $colorwhite;
+  height: 100%;
+}
+.colorname {
+  color: $colorwhite;
+  padding: 20px;
+}
+.image{
+    height: 200px;
+    background: red;
+}
+</style>
