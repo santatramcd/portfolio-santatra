b4a7c5d670813b3fc74e17611639617ec055b4da
santatramcd <andrianjakasantatra22@gmail.com>
2024-06-28 11:48:17 +0300

    first modif icon header

--- src/views/Home/Homeslider.vue
+++ src/views/Home/Homeslider.vue
@@ -7,7 +7,7 @@
             <h1 class="title-show">
               Afficher tous les projets
             </h1>
-            <div class="div-a"><a href="#" @click="work">Click me -></a></div>
+            <div class="div-a"><a href="#" @click="work">Click me ⇨</a></div>
           </div>
         </div>
         <div class="col-lg-6 card">
