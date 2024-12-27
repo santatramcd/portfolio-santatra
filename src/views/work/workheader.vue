aaeb3883a1c068508738926d86209f845ff06d78
santatramcd <andrianjakasantatra22@gmail.com>
2024-12-27 09:40:48 +0300

    remove style and animation page work

--- src/views/work/workheader.vue
+++ src/views/work/workheader.vue
@@ -1,61 +1,102 @@
 <template>
   <div>
     <div class="container">
-      <div>
-        <h1 style="color: #fff">Les sites que j'ai déjà créés par :</h1>
-      </div>
-      <div>
-        <ul class="ul-list-proj">
-          <li>
-            <a href="#" @click.prevent="filterProjects('Tous les projets')"
-              >Tous les projets</a
-            >
-          </li>
-          <li>
-            <a href="#" @click.prevent="filterProjects('WordPress')"
-              >WordPress</a
-            >
-          </li>
-          <li>
-            <a href="#" @click.prevent="filterProjects('Vuejs 3')">Vuejs 3</a>
-          </li>
-          <li>
-            <a href="#" @click.prevent="filterProjects('React Js')">React Js</a>
-          </li>
-          <li>
-            <a href="#" @click.prevent="filterProjects('Application')"
-              >Application</a
-            >
-          </li>
-          <li>
-            <a href="#" @click.prevent="filterProjects('grav')">Grav Cms</a>
-          </li>
-        </ul>
-      </div>
+      <h1 style="color: #fff; text-transform: uppercase">Mes réalisations :</h1>
+      <ul class="ul-list-proj">
+        <li v-for="tech in technologies" :key="tech">
+          <a href="javascript:void(0)" @click.prevent="filterProjects(tech)">
+            {{ tech }}
+          </a>
+        </li>
+      </ul>
       <transition-group name="list" tag="div" class="row div-scroll">
         <div
           class="col-md-4 mt-3"
           v-for="item in filteredData"
           :key="item.link"
         >
-          <a :href="item.link">
-            <div class="divcard">
-              <div class="image">
-                <img :src="item.img" alt="icon" />
-              </div>
-              <div class="colorname">
+          <div class="divcard">
+            <div class="image">
+              <img :src="item.img" :alt="item.title" />
+            </div>
+            <div class="colorname">
+              <div class="hover-div">
+                <span class="element"></span>
+                <div class="links">
                 <h3 class="title-card">{{ item.title }}</h3>
+                  <span class="span-link">
+                    <a :href="item.link" target="_blank">
+                      <i class="bi bi-link-45deg"></i>
+                    </a>
+                  </span>
+
+                  <span class="span-link">
+                    <a href="javascript:void(0)" @click="openModal(item)">
+                      <i class="bi bi-plus"></i>
+                    </a>
+                  </span>
+                </div>
               </div>
             </div>
-          </a>
+          </div>
         </div>
       </transition-group>
+    </div>
+
+    <!-- Modal -->
+    <div
+      class="modal fade bg-dark"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+      ref="modal"
+    >
+      <div class="modal-dialog modal-fullscreen">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" style="text-transform: uppercase" id="exampleModalLabel">
+              {{ selectedItem?.title || "Projet" }}
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <div
+              id="carouselExampleControls"
+              class="carousel slide"
+              data-bs-ride="carousel"
+            >
+              <div class="carousel-inner">
+                <div
+                  v-for="(img, index) in selectedItem?.images || [
+                    selectedItem?.img,
+                  ]"
+                  :key="index"
+                  :class="['carousel-item', { active: index === 0 }]"
+                >
+                  <img
+                    :src="img"
+                    class="d-block w-100"
+                    :alt="selectedItem?.title"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
+// import { ref, computed } from "vue";
 import artiist from "../../assets/image/artiist.png";
 import maki from "../../assets/image/maki.png";
 import createak from "../../assets/image/createak.png";
@@ -72,73 +113,73 @@ import eva from "../../assets/image/eva-car.png";
 const data = [
   {
     img: Monsite,
-    title: "https://santatra-michado.com",
+    title: "Santatra Michado",
     link: "https://santatra-michado.com",
     technology: "WordPress",
   },
   {
     img: Mda,
-    title: "https://mada-digital-agency.com",
+    title: "Mada Digital Agency",
     link: "https://mada-digital-agency.com",
     technology: "WordPress",
   },
   {
     img: artiist,
-    title: "https://www.artiist.fr",
+    title: "artiist",
     link: "https://www.artiist.fr/",
     technology: "WordPress",
   },
   {
     img: createak,
-    title: "https://createak.mu",
+    title: "Createak",
     link: "https://createak.mu/",
     technology: "WordPress",
   },
   {
     img: robin,
-    title: "https://robinruth-thailand.com",
+    title: "robinruth thailand",
     link: "https://robinruth-thailand.com/",
     technology: "WordPress",
   },
   {
     img: maki,
-    title: "https://maki-car-rental-mada.netlify.app",
+    title: "Maki car rental",
     link: "https://maki-car-rental-mada.netlify.app/",
     technology: "Vuejs 3",
   },
   {
     img: eva,
-    title: "https://eva-car-rental.vercel.app",
+    title: "eva car rental",
     link: "https://eva-car-rental.vercel.app/",
     technology: "Vuejs 3",
   },
   {
     img: vueprojet,
-    title: "https://pinkscrap2.netlify.app",
+    title: "pinkscrap2",
     link: "https://pinkscrap2.netlify.app/",
     technology: "Vuejs 3",
   },
   {
     img: projet,
-    title: "https://projet-santatra.netlify.app",
+    title: "Projet",
     link: "https://projet-santatra.netlify.app/",
     technology: "React Js",
   },
   {
     img: ReactJs,
-    title: "https://projet-react-js-pi.vercel.app",
+    title: "Portfolio",
     link: "https://projet-react-js-pi.vercel.app/",
     technology: "React Js",
   },
   {
     img: Kcal,
-    title: "Calculateur-de-calories",
+    title: "Calculateur de calories",
     link: "https://santatramcd.github.io/calculateur-de-calories/",
     technology: "Application",
   },
   {
     img: Lios,
-    title: "https://lios-collection.mu",
+    title: "lios collection",
     link: "https://lios-collection.mu/",
     technology: "grav",
   },
@@ -156,9 +197,37 @@ const filteredData = computed(() => {
 function filterProjects(technology) {
   selectedTechnology.value = technology;
 }
+
+const technologies = [
+  "Tous les projets",
+  "WordPress",
+  "Vuejs 3",
+  "React Js",
+  "Application",
+  "grav",
+];
+const selectedItem = ref(null);
+
+// Calculer les données filtrées en fonction de la technologie sélectionnée
+
+// Fonction pour filtrer les projets
+// Fonction pour ouvrir le modal et afficher les détails du projet sélectionné
+function openModal(item) {
+  selectedItem.value = item;
+  const modalElement = document.getElementById("exampleModal");
+  const modal = new bootstrap.Modal(modalElement);
+  modal.show();
+}
+
+onMounted(() => {
+  // Vérification de l'intégration Bootstrap
+  if (!window.bootstrap) {
+    console.error("Bootstrap JS n'est pas chargé !");
+  }
+});
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .ul-list-proj li {
   display: inline;
   margin-right: 10px;
@@ -191,21 +260,66 @@ a {
   color: #fff;
   text-decoration: none;
 }
+.hover-div {
+  background: rgba(0, 0, 0, 0.6);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: center;
+  justify-content: center;
+}
+.element::before {
+  display: block;
+  content: "";
+  width: 48px;
+  height: 48px;
+  top: 15px;
+  left: 15px;
+  position: absolute;
+  border-top: 3px solid #fff;
+  border-left: 3px solid #fff;
+  transition: all 0.5s ease 0s;
+  z-index: 9994;
+}
+.element::after {
+  display: block;
+  content: "";
+  width: 48px;
+  height: 48px;
+  position: absolute;
+  bottom: 15px;
+  right: 15px;
+  border-bottom: 3px solid #fff;
+  border-right: 3px solid #fff;
+  transition: all 0.5s ease 0s;
+  z-index: 9994;
+}
+
+.colorname {
+  background-color: rgba(0, 0, 0, 0.6);
+  border-radius: 0 0 5px 5px;
+  position: absolute;
+  color: #fff;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  transform: scale(0.8);
+  transition: all 0.3s ease;
+}
+
 .divcard {
-  will-change: transform;
-  -webkit-transition: -webkit-transform 0.7s cubic-bezier(0.2, 1, 0.2, 1);
-  transition: -webkit-transform 0.7s cubic-bezier(0.2, 1, 0.2, 1);
-  -o-transition: transform 0.7s cubic-bezier(0.2, 1, 0.2, 1);
-  transition: transform 0.7s cubic-bezier(0.2, 1, 0.2, 1);
-  transition: transform 0.7s cubic-bezier(0.2, 1, 0.2, 1),
-    -webkit-transform 0.7s cubic-bezier(0.2, 1, 0.2, 1);
-  -webkit-transform: translateY(0);
-  -ms-transform: translateY(0);
-  transform: translateY(0);
+  position: relative;
+  background-color: red;
+  transition: transform 0.3s ease; /* Ajout pour l'animation */
 }
-.divcard:hover {
-  transform: translateY(-10px);
+
+.divcard:hover .colorname {
+  opacity: 1; /* Affiche l'élément lors du survol */
+  transform: scale(1); /* Passe à la taille normale pour l'effet zoom-in */
 }
+
 .image {
   height: 300px;
 }
@@ -221,29 +335,25 @@ a {
 .title-card {
   font-size: 1.2em;
   margin-top: 10px;
-}
-
-.colorname {
-  /* background-color: #f9f9f9;
-  padding: 10px;
-  border-top: 1px solid #ddd;
-  border-radius: 0 0 5px 5px; */
+  text-transform: capitalize;
+  text-align: center;
 }
 
 /* Transition styles */
 .list-enter-active,
 .list-leave-active {
-  transition: all 0.5s ease;
+  transition: all 0.3s ease;
 }
 
 .list-enter-from,
 .list-leave-to {
   opacity: 0;
-  transform: translateY(30px);
+  /* transform: translateY(30px); */
+  transform: scale(0.8);
 }
 .div-scroll {
   max-height: 725px;
-  overflow-y: scroll; /* Permet le défilement */
+  overflow-y: auto; /* Permet le défilement */
   scrollbar-width: thin; /* Réduit la largeur de la barre pour Firefox */
   scrollbar-color: #45f3ff transparent; /* Couleur de la barre et du fond pour Firefox */
 }
@@ -261,8 +371,17 @@ a {
 .div-scroll::-webkit-scrollbar-track {
   background-color: transparent; /* Couleur de l'arrière-plan */
 }
-
-
+.span-link {
+  // background: #45f3ff;
+  display: inline-block;
+  font-size: 2em;
+  text-align: center;
+  width: 100%;
+  max-height: 38px;
+}
+a:hover {
+  color: #45f3ff;
+}
 @media (max-width: 470px) {
   .ul-list-proj {
     display: flex;
